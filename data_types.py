62f9f0a0739950f796224e023d9503f06f066e28
Maria <mariiaadam4uk@gmail.com>
2022-08-09 13:30:22 +0300

    "my first script"

---
+++ data_types.py
@@ -0,0 +1,34 @@
+summer_house_1 : int = 23
+summer_house_2 : str = "23"
+print(summer_house_1, summer_house_1)
+
+roland_center= 0
+roland_center_3 : int= 9
+roland_center_4 : str= "10"
+print(roland_center_3, roland_center_4)
+
+email= 11
+my_personal_mail_a = 2+1
+my_personal_mail_b = 2+4
+print(my_personal_mail_a, my_personal_mail_b)
+
+my_personal_mail_a_1 = 3
+my_personal_mail_b_1 = 6
+print(id(3))
+print(id(6))
+print(id(my_personal_mail_a_1))
+print(id(my_personal_mail_b_1))
+
+variable_1 : int = 135
+variable_2 : str = "136"
+print(variable_1, variable_2)
+
+
+
+
+
+
+
+
+
+
