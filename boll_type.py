3a63d83b667470c160cfc82dc3315e7c29284331
Maria <mariiaadam4uk@gmail.com>
2022-08-15 16:39:16 +0300

    "boll_type.py"

---
+++ boll_type.py
@@ -0,0 +1,37 @@
+comparison_result: bool = 3 > 5
+print(comparison_result)
+
+not False== 5_5
+print(not False)
+True and True== 5_5
+print(True and True)
+True or True == 5_5
+print(True or True)
+True or False == 5_5
+print(True or False)
+False or True== 5_5
+print(False or True)
+
+(True or True) + False==True
+True - (True and False)==True
+not True or True and False==True
+True and True or False==True
+not True or True and False== True
+
+bool_none=bool(7)
+print(bool_none)
+
+variable="value"
+this_calls_none =print(variable)
+print(this_calls_none)
+
+sum_result=10-1
+print(sum_result)
+
+dont_understand=True or False
+print(dont_understand)
+
+
+
+
+
