1206ed4df37767b51cdc624958004a0221d32da9
Maria <mariiaadam4uk@gmail.com>
2022-08-22 16:42:12 +0300

    "my first script"

---
+++ rows_and_numbers.py
@@ -0,0 +1,28 @@
+name = "Mariia"
+print(f"Hello, {name}." )
+
+
+number_example = 10.1
+value = float("10.1")
+print(value, type(value))
+
+value_1 = int(10.1)
+print(value_1, type(value_1))
+
+exponentiation = value_1 ** 4
+print(exponentiation, type(exponentiation))
+
+square_root = value_1 ** 0,5
+print(square_root, type(square_root))
+
+division_remainder = value_1 % 2
+print( division_remainder, type(division_remainder))
+
+
+
+
+
+
+
+
+
