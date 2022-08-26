1779212d99750db418a7c86070e1b341ecb9adef
Maria <mariiaadam4uk@gmail.com>
2022-08-26 10:37:20 +0300

    "rows and numbers 1&2"

---
+++ rows_and_numbers_advance.py
@@ -0,0 +1,26 @@
+name = "Boris  Johnson"
+s1 = "Boris Johnson"
+s2 = s1.swapcase()
+print(s2)
+
+str = " Boris Johnson "
+name_1 = str.replace(" ", "")
+print(name)
+
+name = 'Boris'
+sentence = 'Boris, Hello'
+string = sentence.capitalize()
+print(string)
+
+name = 'Boris'
+str = len(name)
+print(str)
+
+
+str_for_slice = "Boris"
+print("reversed string --> ", str_for_slice[::-1])
+
+
+
+
+
