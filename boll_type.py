bc0a3ba88275a6e26870df0f95c039a7694dbe12
Maria <mariiaadam4uk@gmail.com>
2022-09-26 15:06:49 +0300

    "boll_type"

--- boll_type.py
+++ boll_type.py
@@ -1,32 +1,43 @@
-comparison_result: bool = 3 > 5
-print(comparison_result)
+# comparison_result: bool = 3 < 5
+# print(comparison_result)
+# #
+# # comparison : bool = 5 > 5
+# # print(comparison)
+# #
+# # comparison_1 : bool = 5 != 5
+# # print(comparison_1)
+#
+# comparison_2 : bool = 5 <= 5
+# comparison_3 : bool = 5 >= 5
+# print(comparison_2, "/n", comparison_3)
+#
+#
+#
 
-not False== 5_5
-print(not False)
-True and True== 5_5
-print(True and True)
-True or True == 5_5
-print(True or True)
-True or False == 5_5
-print(True or False)
-False or True== 5_5
-print(False or True)
+result = True and True or False #був True
+result_1 = (True and True) or not False
+result_2 = not True or True and not False
+result_3 = True or True and not False
+result_4 = True and (True or False)
+print(result_4)
+print(result_3)
+print(result_2)
+print(result_1)
 
-(True or True) + False==True
-True - (True and False)==True
-not True or True and False==True
-True and True or False==True
-not True or True and False== True
+bool_none = "bool_non"
+another_result = print(7)
+print(another_result, type(another_result), id(another_result))
+print(another_result == another_result)
 
-bool_none=bool(7)
-print(bool_none)
 
-variable="value"
-this_calls_none =print(variable)
-print(this_calls_none)
+sum_str = " "
+sum_result = 10 - 1
+print (sum_str, sum_result, sum_result == sum_str)
 
-sum_result=10-1
-print(sum_result)
+sum_bool = True or False
+print(sum_bool)
+h = print("Today is rainy")
+print(sum_bool == h )
 
-dont_understand=True or False
-print(dont_understand)
+
+
