d8e633e2303cb566bb8c8c5b2e87a9544ade0973
Maria <mariiaadam4uk@gmail.com>
2022-09-26 15:10:46 +0300

    "boll_type"

--- boll_type.py
+++ boll_type.py
@@ -1,18 +1,15 @@
-# comparison_result: bool = 3 < 5
-# print(comparison_result)
-# #
-# # comparison : bool = 5 > 5
-# # print(comparison)
-# #
-# # comparison_1 : bool = 5 != 5
-# # print(comparison_1)
-#
-# comparison_2 : bool = 5 <= 5
-# comparison_3 : bool = 5 >= 5
-# print(comparison_2, "/n", comparison_3)
-#
-#
-#
+comparison_result: bool = 3 < 5
+print(comparison_result)
+
+comparison : bool = 5 > 5
+print(comparison)
+
+comparison_1 : bool = 5 != 5
+print(comparison_1)
+
+comparison_2 : bool = 5 <= 5
+comparison_3 : bool = 5 >= 5
+print(comparison_2, "/n", comparison_3)
 
 result = True and True or False #був True
 result_1 = (True and True) or not False
@@ -29,7 +26,6 @@ another_result = print(7)
 print(another_result, type(another_result), id(another_result))
 print(another_result == another_result)
 
-
 sum_str = " "
 sum_result = 10 - 1
 print (sum_str, sum_result, sum_result == sum_str)
@@ -38,6 +34,3 @@ sum_bool = True or False
 print(sum_bool)
 h = print("Today is rainy")
 print(sum_bool == h )
-
-
-
